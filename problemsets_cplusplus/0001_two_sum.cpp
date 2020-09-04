0cf88b39c041d9975f669b88bd1ace39edc5fbd0
hariharanragothaman <hariharanragothaman@gmail.com>
2020-09-03 23:54:07 -0400

    C++ solution for two_sum - Rev mode

---
+++ problemsets_cplusplus/0001_two_sum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target)
+{
+    vector<int> result;
+
+    map<int, int> hmap;
+    int idx = 0;
+
+    for(auto& elem: nums)
+    {
+        hmap[elem] = idx++;
+    }
+
+
+    for(int i=0; i < nums.size(); i++)
+    {
+        if( hmap.count(target - nums[i]) > 0 and i != hmap[target-nums[i]])
+        {
+            result.push_back(i);
+            result.push_back(hmap[target-nums[i]]);
+            return result;
+        }
+    }
+
+}
+
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    vector<int> result;
+    
+    int target = 9;
+    result = twoSum(nums, target);
+
+    int idx = 0;
+    for(auto& elem: result)
+    {
+        cout << elem;
+    }
+
+    return 0;
+}
