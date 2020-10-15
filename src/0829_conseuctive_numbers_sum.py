e9430d1d4bae319459e40ada4e8cbcdc5a29d414
hariharanragothaman <hariharanragothaman@gmail.com>
2020-10-15 01:39:08 -0400

    Consecutive numbers sum.

---
+++ src/0829_conseuctive_numbers_sum.py
@@ -0,0 +1,20 @@
+"""
+Given a positive integer N, how many ways can we write it as a sum of consecutive positive integers?
+"""
+class Solution:
+    def consecutiveNumbersSum(self, N: int) -> int:
+        window = list(range(1, N))
+        csum, count = 0, 0
+        left, right = 0, 0
+
+        while right < len(window):
+            csum += window[right]
+
+            while csum > N:
+                csum -= window[left]
+                left += 1
+
+            if csum == N: count += 1
+            right += 1
+
+        return count + 1
