14c9f0ec7a65d5e3fb7874d4f343be073cb02603
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-04 22:08:43 -0500

    Largest Subarray with length K

---
+++ src/1708_largest_subarray_with_length_k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largestSubarray(self, nums: List[int], k: int) -> List[int]:
+        """
+        Here the core idea is, we can only form subarray of length starting till len(nums)-k+1
+        Since only the first element matters - we only need to start from there
+        This is a greedy approach.
+
+        :param nums:
+        :param k:
+        :return:
+        """
+
+        if k == 1: return [max(nums)]
+
+        hash_map = {}
+        for i, n in enumerate(nums):
+            hash_map[n] = i
+
+        candidates = nums[:len(nums) - k + 1]
+        print(candidates)
+        mx = max(candidates)
+        mx_idx = hash_map[mx]
+        op = nums[mx_idx:mx_idx + k]
+        return op
