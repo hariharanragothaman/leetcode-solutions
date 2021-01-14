2df8f9ab4fd603d419cf59fdfdd99d8a39ef9ff6
hariharanragothaman <hariharanragothaman@gmail.com>
2021-01-14 13:46:50 -0500

    Binary search to figure out - the capacity to ship packages with D days

---
+++ src/1011_capacity_to_ship_packages_within_d_days.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        def condition(capacity) -> bool:
+            days = 1
+            total = 0
+
+            for weight in weights:
+                total += weight
+                if total > capacity:
+                    total = weight
+                    days += 1
+                    if days > D:
+                        return False
+                print("The total is:", total)
+            print("The number of days is:", days)
+            return True
+
+        left, right = max(weights), sum(weights)
+
+        while left < right:
+            mid = left + (right - left) // 2
+            print("The mid value is:", mid)
+            if condition(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
---
+++ src/1704_determine_if_string_halves_are_alike.py
@@ -0,0 +1,54 @@
+"""
+
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+Return true if a and b are alike. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+Example 3:
+
+Input: s = "MerryChristmas"
+Output: false
+Example 4:
+
+Input: s = "AbCdEfGh"
+Output: true
+
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+"""
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        vowels = 'aeiouAEIOU'
+        l = len(s)
+        p1, p2 = s[:l // 2], s[l // 2:]
+        ctr1, ctr2 = Counter(p1), Counter(p2)
+
+        cnt1, cnt2 = 0, 0
+
+        for k, v in ctr1.items():
+            if k in vowels:
+                cnt1 += v
+
+        for k, v in ctr2.items():
+            if k in vowels:
+                cnt2 += v
+
+        return cnt1 == cnt2
