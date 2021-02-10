e682154c70239ff7137b741f95e29eceb67879b7
hariharanragothaman <hariharanragothaman@gmail.com>
2021-02-10 10:02:28 -0500

    Easy straightforward problem - There are probably better ways to do it.

---
+++ src/1742_maximum_number_of_balls_in_box.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        g = defaultdict(int)
+        while lowLimit <= highLimit:
+            s = sum(int(c) for c in str(lowLimit))
+            g[s] += 1
+            lowLimit += 1
+
+        g = {k: v for k, v in sorted(g.items(), key=lambda x: x[1], reverse=True)}
+        return list(g.values())[0]
