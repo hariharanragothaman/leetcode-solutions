712d03b64e25946e44d8aefad48f42e04a1a5d87
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-23 16:56:07 -0400

    Simple 2 pointer solutuon - codeforces style

---
+++ src/0408_valid_word_abbreviation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i = j = 0
+        while j < len(abbr) and i < len(word):
+            if abbr[j].isalpha():
+                if abbr[j] != word[i]:
+                    return False
+                i += 1
+                j += 1
+            else:
+                if abbr[j] == '0':  # to handle edge cases such as "01", which are invalid
+                    return False
+                temp = 0
+                while j < len(abbr) and abbr[j].isdigit():
+                    temp = temp * 10 + int(abbr[j])
+                    j += 1
+                i += temp
+
+        return j == len(abbr) and i == len(word)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    word = "hi"
+    abbr = "1i"
+
+    res = s.validWordAbbreviation(word, abbr)
+    print(res)
