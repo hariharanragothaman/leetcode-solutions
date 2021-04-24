4a37ae533291b6dd21f052ea118cdb0a9cab35fa
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-24 18:56:29 -0400

    Find maximum depth by using raw BFS template

---
+++ src/0104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root:TreeNode) -> int:
+        # Applying level order traversal template
+        if not root:
+            return 0
+
+        q = deque([root])
+        level = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            level += 1
+        return level
