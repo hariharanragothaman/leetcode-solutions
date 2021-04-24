b2d7fc5ae279e5ec0941d80a4fedc23cdd919b58
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-24 15:28:17 -0400

    Solving some easy problems

---
+++ src/0079_word_search.py
@@ -0,0 +1,65 @@
+from typing import List
+
+
+class Trie:
+    def __init__(self, *words):
+        self.root = dict()
+        for word in words:
+            self.add(word)
+
+    def add(self, word):
+        current_dict = self.root
+        if self.__contains__(word):
+            for letter in word:
+                current_dict = current_dict[letter]
+            current_dict["_cnt_"] += 1
+            return
+
+        current_dict = self.root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, dict())
+        current_dict["_end_"] = True
+        current_dict["_cnt_"] = 1
+
+    def __contains__(self, word):
+        current_dict = self.root
+        for letter in word:
+            if letter not in current_dict:
+                return False
+            current_dict = current_dict[letter]
+        return '_end_' in current_dict
+
+    def __delitem__(self, word):
+        current_dict = self.root
+        nodes = [current_dict]
+        for letter in word:
+            current_dict = current_dict[letter]
+            nodes.append(current_dict)
+        del current_dict["_end_"]
+
+
+class Solution:
+    def search_trie(board, trie, result, rows, cols, path=None, node=None, word=None):
+
+        letter = board[rows][cols]
+
+        if node is None or path is None or word is None:
+            pass
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        R, C = len(board), len(board[0])
+        result = set()
+        words = [word]
+        print(words)
+        trie_obj = Trie(*words)
+        print(trie_obj.root)
+
+
+
+
+
+if __name__ == '__main__':
+    board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+    word = "ABCCED"
+    s = Solution()
+    print(s.exist(board, word))
---
+++ src/1832_check_if_sentence_is_pangram.py
@@ -0,0 +1,44 @@
+"""
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+
+
+
+Constraints:
+
+    1 <= sentence.length <= 1000
+    sentence consists of lowercase English letters.
+
+
+
+"""
+
+from collections import Counter
+
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        alpha = [chr(c) for c in range(ord('a'), ord('z') + 1)]
+        print(alpha)
+
+        ctr = Counter(sentence)
+        keys = sorted(ctr.keys())
+
+        if alpha == keys:
+            return True
+        return False
