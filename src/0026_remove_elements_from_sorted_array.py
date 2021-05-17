d158df943e6d26c52698983a4d61f5a505d2aab4
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-16 23:47:26 -0400

    Memory efficient solution

--- src/0026_remove_elements_from_sorted_array.py
+++ src/0026_remove_elements_from_sorted_array.py
@@ -4,6 +4,8 @@ Remove the elements in-place
 """
 # Basic things to remember here - use while instead of for  and use the 'del' operator.
 
+from typing import List
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 1
@@ -13,3 +15,12 @@ class Solution:
             else:
                 i += 1
         return len(nums)
+
+    def removeDuplicatesUpdated(self, nums: List[int]) -> int:
+        # Solution by over-writing
+        write_index = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[write_index - 1]:
+                nums[write_index] = nums[i]
+                write_index += 1
+        return write_index
