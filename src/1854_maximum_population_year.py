c21d43b277b3067369b0079272d66c27e5be1895
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-21 23:36:13 -0400

    Sentence reconstruction simple

--- src/1854_maximum_population_year.py
+++ src/1854_maximum_population_year.py
@@ -2,6 +2,7 @@ from typing import List
 from collections import defaultdict
 import heapq
 
+
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         hmap = defaultdict(int)
@@ -9,5 +10,5 @@ class Solution:
             for i in range(b, d):
                 hmap[i] += 1
         mx = heapq.nlargest(1, list(hmap.values()))[0]
-        candidates = [k for k, v in hmap.items() if v==mx]
+        candidates = [k for k, v in hmap.items() if v == mx]
         return sorted(candidates)[0]
