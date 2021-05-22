c68f1443a28df666454ee1c812dccdb8d0c75880
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-05-22 12:30:44 -0400

    Sentence reconstruction simple

---
+++ src/0985_sum_of_even_numbers_after_queries.py
@@ -0,0 +1,10 @@
+from typing import List
+
+# This solution however TLE's - need to optimize
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        result = []
+        for val, idx in queries:
+            nums[idx] += val
+            result.append(sum(c for c in nums if not c & 1))
+        return result
