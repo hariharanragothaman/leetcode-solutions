74a9ebfbbd84a7a99c157b330f62a541fbad0066
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-06-08 12:00:14 -0400

    Find if continuous subsegments of 1 (or) 0 is greater?

---
+++ src/1869_longer_contiguous_segments_of_ones_than_zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def get_count(self, s, char):
+        n = len(s)
+        max_count = 0
+        tmp = 0
+        for i in range(0, n):
+            if s[i] == char:
+                tmp += 1
+            else:
+                max_count = max(max_count, tmp)
+                tmp = 0
+        max_count = max(max_count, tmp)
+        return max_count
+
+    def checkZeroOnes(self, s: str) -> bool:
+        a = self.get_count(s, "1")
+        b = self.get_count(s, "0")
+        return a > b
