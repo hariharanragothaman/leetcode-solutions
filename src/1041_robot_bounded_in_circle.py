cfb5627303af317dbd1aba4337d0bff2289d92c4
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-06-19 19:43:52 -0400

    Robot bounded in a circle - If robot gets back to origin (or) if he does not face in the forward direction

---
+++ src/1041_robot_bounded_in_circle.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isRobotBounded(self, s: str) -> bool:
+        """
+        start = (0, 0) --> (x, y)
+        2D place
+        L means...
+            whatever G after it
+        R means..
+
+        Starting at the origin and face north (0,1),
+        after one sequence of instructions,
+
+        if chopper return to the origin, he is obvious in an circle.
+        if chopper finishes with face not towards north,
+        it will get back to the initial status in another one or three sequences.
+        """
+        x, y, dx, dy = 0, 0, 0, 1
+        # smart usage of dx, dy to change directions. Fantastic!
+        for c in s:
+            if s == 'R':
+                dx = dy
+                dy = -dx
+            if s == 'L':
+                dx = -dy
+                dy = dx
+            if s == 'G':
+                x += dx
+                y += dx
+
+        return (x, y) == (0,0) or (dx, dy) != (0, 1)
+
+
+if __name__ == '__main__':
+    instructions = "GGLLGG"
+    instructions = input()
+    s = Solution()
+    result = s.isRobotBounded(instructions)
+    print(f"Is the robot bounded? {result}")
