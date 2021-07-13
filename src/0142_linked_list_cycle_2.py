f1f0d0a96619a765cd68a0078e8251779d8a644f
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-07-13 13:12:07 -0400

    Check if linked list has cycle and tell the point where the cycle starts

---
+++ src/0142_linked_list_cycle_2.py
@@ -0,0 +1,44 @@
+from typing import *
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+
+        def getIntersect(head):
+            slow = head
+            fast = head
+
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+                if slow == fast:
+                    return slow
+
+            return None
+
+        if head is None:
+            return None
+
+        # If there is a cycle, the fast/slow pointers will intersect at some
+        # node. Otherwise, there is no cycle, so we cannot find an entrance to
+        # a cycle.
+        intersect = getIntersect(head)
+        if intersect is None:
+            return None
+
+        # To find the entrance to the cycle, we have two pointers traverse at
+        # the same speed -- one from the front of the list, and the other from
+        # the point of intersection.
+        ptr1 = head
+        ptr2 = intersect
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        return ptr1
