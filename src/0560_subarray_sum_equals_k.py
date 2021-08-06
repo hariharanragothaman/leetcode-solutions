539660cdec046af9881c4461b2d72b322eebae6b
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-06 12:16:32 -0400

    Prefix sum + math funda

--- src/0560_subarray_sum_equals_k.py
+++ src/0560_subarray_sum_equals_k.py
@@ -3,18 +3,22 @@ from collections import Counter
 
 # Remmeber Counter() remembers the order
 
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+
         n = len(nums)
         prefix_sum = [0] * (n + 1)
-        hashmap = Counter()
-        hashmap[0] = 1
-        count = 0
 
+        # Get the prefix sum
         for i in range(1, n + 1):
-            value = prefix_sum[i - 1] + nums[i - 1]
-            prefix_sum[i] = value
-            count += hashmap.get(value - k, 0)
-            hashmap[value] += 1
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1]
 
-        return count
+        ctr = Counter()
+        ctr[0] = 1
+        ans = 0
+
+        for i in range(1, len(prefix_sum)):
+            ans += ctr.get(prefix_sum[i] - k, 0)
+            ctr[prefix_sum[i]] += 1
+        return ans
