2b26e8da55fcc923259824555d467fae97e0941e
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-09-05 07:58:26 -0400

    Auto-formatting clean

--- src/1570_dot_products_of_two_sparse_vectors.py
+++ src/1570_dot_products_of_two_sparse_vectors.py
@@ -1,11 +1,12 @@
 from typing import *
 
+
 class SparseVector:
     def __init__(self, nums: List[int]):
         self.v1 = nums
 
     # Return the dotProduct of two sparse vectors
-    def dotProduct(self, vec: 'SparseVector') -> int:
+    def dotProduct(self, vec: "SparseVector") -> int:
         self.ans = 0
         for c in zip(self.v1, vec.v1):
             if c[0] != 0 and c[1] != 0:
--- src/1762_buildings_with_a_view.py
+++ src/1762_buildings_with_a_view.py
@@ -1,11 +1,12 @@
 from typing import *
 
+
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         n = len(heights)
         minx = heights[-1]
-        res = [n-1]
-        for i in range(n-2, -1, -1):
+        res = [n - 1]
+        for i in range(n - 2, -1, -1):
             if heights[i] > minx:
                 minx = heights[i]
                 res.append(i)
