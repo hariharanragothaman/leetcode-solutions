510dd18383f95b289e7770f62d905ee031675ba4
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-10-14 15:52:56 -0400

    Sorting and greedily taking boxes since we want to maximize

---
+++ src/1710_maximum_units_on_a_truck.py
@@ -0,0 +1,16 @@
+from typing import *
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes = sorted(boxTypes, key=lambda x:x[1], reverse=True)
+        total = 0
+        curr_boxes = 0
+        for box_count, units in boxTypes:
+            if curr_boxes + box_count < truckSize:
+                total += box_count * units
+                curr_boxes += box_count
+            else:
+                remaining = truckSize - curr_boxes
+                total +=  remaining * units
+                curr_boxes += remaining
+        return total
